e79ebb83915d4dd332d3b2749af7dfd4b5c9ba55
Neal <groessler24@gmail.com>
2015-12-10 20:59:03 -0800

    dynamo

--- application.py
+++ application.py
@@ -1,15 +1,22 @@
 import os, boto
 from boto.s3.key import Key
 from flask import Flask, request, redirect, url_for, render_template
+from flask.ext.dynamo import Dynamo
 from urllib2 import Request, urlopen, URLError
+from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.table import Table
 
 #TODO: DYNAMO http://flask-dynamo.readthedocs.org/en/latest/quickstart.html
 
 S3BUCKET = 'isithappeningpictures'
 
-application = Flask(__name__)
+app = Flask(__name__)
+app.config['DYNAMO_TABLES'] = [
+    Table('Locations', schema=[HashKey('LocationName')]),
+    Table('Pictures', schema=[HashKey('PictureId')]),
+]
 
-@application.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Connect to Amazon S3
@@ -25,7 +32,14 @@ def index():
             k.set_contents_from_string(file_contents)
 
     return render_template('index.html')
+	
+@app.route('/create_location')
+def create_location():
+    dynamo.Locations.put_item(data={
+        'LocationName': 'unicorn',
+        'Address': '123 cap hill',
+    })
 
 if __name__ == "__main__":
-    application.debug = True
-    application.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')
